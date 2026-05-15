ee52172b904d9d368013ffac04914bc6227e40db
nebiros88 <nebiros1988@gmail.com>
2026-05-15 12:14:30 +0200

    task-5: fix permission for lambda file parser

--- infra/lib/backend-nodejs-aws-shop-react-product-service-stack.ts
+++ infra/lib/backend-nodejs-aws-shop-react-product-service-stack.ts
@@ -162,7 +162,7 @@ export class BackendNodejsAwsShopReactProductServiceStack extends cdk.Stack {
 
     // grant IAM permissions to access S3Bucket
     importBucket.grantPut(importProductsFileLambda);
-    importBucket.grantPut(importFileParserLambda);
+    importBucket.grantRead(importFileParserLambda);
 
     // S3Bucket event configuration
     importBucket.addEventNotification(
