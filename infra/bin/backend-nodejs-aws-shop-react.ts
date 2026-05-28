c145cf8ba421be9c6b7cb139837263493f3b1341
nebiros88 <nebiros1988@gmail.com>
2026-05-28 13:49:58 +0200

    task-7: rollback product service stack renaming, cdk:deploy script update - add flag

--- infra/bin/backend-nodejs-aws-shop-react.ts
+++ infra/bin/backend-nodejs-aws-shop-react.ts
@@ -19,7 +19,7 @@ const authorizationServiceStack =
 
 const productServiceStack = new BackendNodejsAwsShopReactProductServiceStack(
   app,
-  'BackendNodejsAwsShopReactProductServiceStack',
+  'BackendNodejsAwsShopReactStack',
   {
     /* If you don't specify 'env', this stack will be environment-agnostic.
      * Account/Region-dependent features and context lookups will not work,
